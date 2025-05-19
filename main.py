e2d9db84a78cb5e07f1d7c39ec2c064784e75c96
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-19 15:40:32 -0300

    style: unique features

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
@@ -18,8 +18,8 @@ plt.title('Relationship between Call SAC and Churn')
 plt.show()
 
 # Clustering for customer segmentation
-features_cluster = ['subscription-time', 'plan-type', 'use-service', 'call_sac', 'client-satisfaction', 'complaints']
-X_cluster = df[features_cluster]
+features = ['subscription-time', 'plan-type', 'use-service', 'call_sac', 'client-satisfaction', 'complaints']
+X_cluster = df[features]
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X_cluster)
 
@@ -51,11 +51,10 @@ plt.ylabel('Componente PCA 2')
 plt.show()
 
 # Pre-processing for classification model
-features = ['subscription-time', 'plan-type', 'use-service', 'call_sac', 'client-satisfaction', 'complaints', 'cluster']
-X = df[features]
+X = df[features + ['cluster']]
 y = df['churn']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, text_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 rf = RandomForestClassifier(n_estimators=100, random_state=42)
 rf.fit(X_train, y_train)
