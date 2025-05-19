84f02cd3e39b162a3d4df1dc6169d0fdfb3f44f6
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-19 11:06:16 -0300

    feat: add DataFrame

--- dataframe.py
+++ dataframe.py
@@ -6,4 +6,18 @@ import seaborn as sns
 sns.set_style('whitegrid')
 plt.rcParams['figure.figsize'] = (12, 8)
 
+np.random.seed(42)
+n_clients = 1000
 
+data = {
+    'client_id': range(1, n_clients + 1),
+    'subscription-time': np.random.randint(1, 37, n_clients),
+    'plan-type': np.random.choice(['Basic', 'Premium', 'Gold'], n_clients),
+    'use-service': np.random.normal(50, 15, n_clients).round(1),
+    'call_sac': np.random.poisson(5, n_clients),
+    'client-satisfaction': np.random.randint(1, 6, n_clients),
+    'complaints': np.random.randint(0, 10, n_clients),
+    'churn': np.random.choice([0, 1], n_clients, p=[0.7, 0.3])
+}
+
+df = pd.DataFrame(data)
