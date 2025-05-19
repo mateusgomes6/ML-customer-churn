7c6f258c1e0c805841355a368deb877818b6b1a5
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-19 12:00:28 -0300

    feat: preparing clustering

--- main.py
+++ main.py
@@ -16,3 +16,8 @@ plt.figure(figsize=(10, 6))
 sns.boxplot(x='churn', y='call_sac', data=df)
 plt.title('Relação entre Churn e Ligações para o SAC')
 plt.show()
+
+features_cluster = ['subscription-time', 'plan-type', 'use-service', 'call_sac', 'client-satisfaction', 'complaints']
+X_cluster = df[features_cluster]
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X_cluster)
