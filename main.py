d494bbac25bd60f4992d25dbebd8914a3c6d24b4
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-19 11:41:45 -0300

    feat: import librarys

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from dataframe import df
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-print(df.head(2))
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.decomposition import PCA
+
+from dataframe import df
